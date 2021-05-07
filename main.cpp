a7c91557bd658437d970f0a58f536bc786712fef
HiginioJr <littleram100@gmail.com>
2021-05-06 19:11:04 -0700

    a points tester
    a basic randomizer
    
    fixed basic problems on my pc
    
    added an exit and a loop
    
    fixed the exit
    
    just added #include tetris.h
    
    change line clear to layer clear
    
    just added and corrected some of the loop
    
    added a commit on break loop

--- main.cpp
+++ main.cpp
@@ -12,13 +12,13 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.show();
 
-    // Due to personal preference, I have these lines set the window to fullscreen. If you don't want the window to be that large, feel free to comment this out.
+//    // Due to personal preference, I have these lines set the window to fullscreen. If you don't want the window to be that large, feel free to comment this out.
     QDesktopWidget widget; // Gets info about the desktop. I have multiple monitors, so I need to make sure that my primary screen is the one that the game is shown on.
     w.setGeometry(widget.screenGeometry(widget.primaryScreen())); // Ignore the warnings about this being depricated, I couldn't get it to work in the way that it suggests
     w.setVisibility(QWindow::FullScreen); //Makes the window go fullscreen
-    w.resize(widget.screenGeometry(widget.primaryScreen()).x(), widget.screenGeometry(widget.primaryScreen()).y()); // I really don't think this is neccessary, but its here anyway
+//    w.resize(widget.screenGeometry(widget.primaryScreen()).x(), widget.screenGeometry(widget.primaryScreen()).y()); // I really don't think this is neccessary, but its here anyway
 
-    w.setMouseGrabEnabled(true);
+//    w.setMouseGrabEnabled(true);
 
     if (w.visibility() != 5) {
         w.resize(960, 540);
